b60a6aa50ca73a25dac67d42d680379323d61da4
Nikson-A <43266690+Nikson-A@users.noreply.github.com>
2022-08-19 01:56:53 +0530

    Update ut.py

--- core/ut.py
+++ core/ut.py
@@ -9,16 +9,18 @@ from util.git_engine import get_git_diff
 
 def unit_test_executor():
     git_diff = get_git_diff()
+    print(git_diff)
     for dir in git_diff:
         # _cd = cd.get_current_dir()
         # print(_cd, type(_cd))
         # os.system("export PYTHONPATH=$PYTHONPATH:%s" %_cd)
         # sys.path.append(cd.get_current_dir())
         # os.system("echo $PYTHONPATH")
-        with cd(dir):
-            # os.system("pwd")
-            # subprocess.run(["nosetests", "-x", "--with-coverage", "--cover-erase", "--cover-package=.", "--cover-tests",
-            #                 "--cover-xml"])
-            # print(coverage_export(cd.get_current_dir()))
-            notify = Postman()
-            notify.send(cd.get_current_dir())
+        if not os.path.ifile(os.getcwd() + "/" + dir):
+            with cd(dir):
+                # os.system("pwd")
+                subprocess.run(["nosetests", "-x", "--with-coverage", "--cover-erase", "--cover-package=.", "--cover-tests",
+                                "--cover-xml"])
+                # print(coverage_export(cd.get_current_dir()))
+                notify = Postman()
+                notify.send(cd.get_current_dir())
