6891c04f43b14492f05069de70a7f95ce1174191
Nikson-A <43266690+Nikson-A@users.noreply.github.com>
2022-08-18 23:31:53 +0530

    Create ut.py

---
+++ core/ut.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import subprocess
+from Notification.postman import Postman
+from util.change_directory import ChangeDirectory as cd
+from util.coverage_parser import coverage_export
+from util.git_engine import get_git_diff
+
+
+def unit_test_executor():
+    git_diff = get_git_diff()
+    for dir in git_diff:
+        # _cd = cd.get_current_dir()
+        # print(_cd, type(_cd))
+        # os.system("export PYTHONPATH=$PYTHONPATH:%s" %_cd)
+        # sys.path.append(cd.get_current_dir())
+        # os.system("echo $PYTHONPATH")
+        with cd(dir):
+            # os.system("pwd")
+            # subprocess.run(["nosetests", "-x", "--with-coverage", "--cover-erase", "--cover-package=.", "--cover-tests",
+            #                 "--cover-xml"])
+            # print(coverage_export(cd.get_current_dir()))
+            notify = Postman()
+            notify.send(cd.get_current_dir())
