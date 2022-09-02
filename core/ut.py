6ebe78730888820a02e2b2911482b6d3123e5946
Nikson-A <43266690+Nikson-A@users.noreply.github.com>
2022-09-02 10:09:00 +0530

    Update ut.py

--- core/ut.py
+++ core/ut.py
@@ -4,25 +4,23 @@ import subprocess
 from Notification.postman import Postman
 from util.change_directory import ChangeDirectory as cd
 from util.coverage_parser import coverage_export
+import json
 from util.git_engine import get_git_diff
 
 
 def unit_test_executor():
     git_diff = get_git_diff()
+    ut_conf = json.loads(os.genv("ut_conf"))
     print(git_diff)
     block = True
-    for dir in git_diff:
-        # _cd = cd.get_current_dir()
-        # print(_cd, type(_cd))
-        # os.system("export PYTHONPATH=$PYTHONPATH:%s" %_cd)
-        # sys.path.append(cd.get_current_dir())
+    for _conf in ut_conf:
+        _cd = cd.get_current_dir()
+        os.system("export PYTHONPATH=$PYTHONPATH:%s" %_cd + ut_conf["python_path"])
         # os.system("echo $PYTHONPATH")
-        if not dir.startswith(".") and not os.path.isfile(os.getcwd() + "/" + dir):
+        if _conf.get("path")[".split("/")[0] in git_diff:
             with cd(dir):
-                os.system("if [ -f requirements.txt ]; then pip install -r requirements.txt; fi")
-                subprocess.run(["nosetests", "-x", "--with-coverage", "--cover-erase", "--cover-package=.", "--cover-tests",
-                                "--cover-xml"])
-                # print(coverage_export(cd.get_current_dir()))
+                os.system(f"if [ -f {_conf["requirement"]} ]; then pip install -r _conf["requirement"]; fi")
+                subprocess.run(["nosetests", "-x", "--with-coverage", "--cover-erase", "--cover-package=.", "--cover-tests", "--cover-xml"])
                 notify = Postman()
                 if not notify.send(cd.get_current_dir()):
                     block = False
