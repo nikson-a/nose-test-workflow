d008160d010d12bdd0408cf210a8a0655d44b8eb
Nikson-A <43266690+Nikson-A@users.noreply.github.com>
2022-09-02 10:12:24 +0530

    Update ut.py

--- core/ut.py
+++ core/ut.py
@@ -19,7 +19,7 @@ def unit_test_executor():
         # os.system("echo $PYTHONPATH")
         if _conf.get("path").split("/")[0] in git_diff:
             with cd(dir):
-                os.system(f"if [ -f {_conf["requirement"]} ]; then pip install -r _conf["requirement"]; fi")
+                os.system(f"if [ -f {_conf['requirement']} ]; then pip install -r _conf['requirement']; fi")
                 subprocess.run(["nosetests", "-x", "--with-coverage", "--cover-erase", "--cover-package=.", "--cover-tests", "--cover-xml"])
                 notify = Postman()
                 if not notify.send(cd.get_current_dir()):
