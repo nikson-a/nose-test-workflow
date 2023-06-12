280fa48f9d6b3a3986c4471d5a1534cc7ee8132c
Nikson <nikson@Admins-MacBook-Pro-27.local>
2023-06-12 20:25:06 +0530

    init

--- core/ut.py
+++ core/ut.py
@@ -20,7 +20,9 @@ def unit_test_executor():
     block = True
     for _conf in ut_conf:
         _cd = cd.get_current_dir()
-        os.system("export PYTHONPATH=$PYTHONPATH:%s" %_cd + _conf.get("python_path", ""))
+        os.system("echo $GITHUB_WORKSPACE")
+        os.system("export PYTHONPATH=$PYTHONPATH:$GITHUB_WORKSPACE:%s" %_cd + _conf.get("python_path", ""))
+        os.system("echo $PYTHONPATH")
         if _conf.get("path").split("/")[0] in git_diff:
             with cd(_conf.get("path")):
                 os.system(f"if [ -f {_conf['requirement']} ]; then pip install -r {_conf['requirement']}; fi")
